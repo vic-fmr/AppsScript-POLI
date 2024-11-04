fff2fd7a89804cff9b6d8f87d0e34363e652cd5e
victor <vfm@poli.br>
2024-11-04 09:36:56 -0300

    modularizacao

--- verificar-duplicidade.js
+++ verificar-duplicidade.js
@@ -1,38 +1,45 @@
 //Script para verificar casos de cpfs cadastrados em 2 estágios classificados com "EM ANDAMENTO" simultanemente
 
-function verificarDuplicidade() {
-    let planilha = SpreadsheetApp.getActiveSpreadsheet()
-    let aba_matriz = planilha.getSheetByName("CONTROLE - 2022.2");
-    let aba_cpfs_duplos = planilha.getSheetByName("Dados Para Corrigir na Matriz")
+function coletarDados() {
+  let planilha = SpreadsheetApp.getActiveSpreadsheet()
+  let aba_matriz = planilha.getSheetByName("CONTROLE - 2022.2");
+  let aba_cpfs_duplos = planilha.getSheetByName("Dados Para Corrigir na Matriz")
+
+  let linhaInicial = aba_cpfs_duplos.getRange(1, 6).getValue();
+  let ultimaLinha = aba_matriz.getLastRow() - (linhaInicial - 1);
   
-    let cpfs = aba_matriz.getRange(5, 5, aba_matriz.getLastRow() - 4, 1).getValues()
-    let status = aba_matriz.getRange(5, 40, aba_matriz.getLastRow() - 4, 1).getValues()
-  
-  
-  
-    let cpfs_duplos = []
-  
-    for (i = 0; i < cpfs.length; i++) {
-  
-      let em_andamento = 0
-      
-      for (n = 0; n < status.length; n++) {
-  
-        if (cpfs[n][0] == cpfs[i][0] && status[n][0] == "EM ANDAMENTO") {
-          em_andamento++
-          
-  
-          if (em_andamento > 1) {
-            cpfs_duplos.push(cpfs[i][0])
-            break;
-          }
+  let cpfs = aba_matriz.getRange(linhaInicial, 5, ultimaLinha, 1).getValues()
+  let status = aba_matriz.getRange(linhaInicial, 40, ultimaLinha, 1).getValues()
+
+  verificarDuplicidade(aba_cpfs_duplos, cpfs, status)
+}
+
+function verificarDuplicidade(aba_cpfs_duplos, cpfs, status) {
+  let cpfs_duplos = []
+
+  for (i = 0; i < cpfs.length; i++) {
+
+    let em_andamento = 0
+
+    for (n = 0; n < status.length; n++) {
+
+      if (cpfs[n][0] == cpfs[i][0] && status[n][0] == "EM ANDAMENTO") {
+        em_andamento++
+
+
+        if (em_andamento > 1) {
+          cpfs_duplos.push(cpfs[i][0])
+          break;
         }
       }
     }
-  
-    if (cpfs_duplos.length > 0) {
-    
-    let cpfs_duplos_2D = cpfs_duplos.map(cpf => [cpf]); 
-    aba_cpfs_duplos.getRange(2, 1, cpfs_duplos_2D.length, 1).setValues(cpfs_duplos_2D);
   }
+  guardarNaAba(aba_cpfs_duplos, cpfs_duplos)
+}
+function guardarNaAba(aba_cpfs_duplos, cpfs_duplos) {
+  if (cpfs_duplos.length > 0) {
+
+    let cpfs_duplos_2D = cpfs_duplos.map(cpf => [cpf]);
+    aba_cpfs_duplos.getRange(4, 1, cpfs_duplos_2D.length, 1).setValues(cpfs_duplos_2D);
   }
+}
